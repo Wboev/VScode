25ba8451387c3a1d37e1b90e062cccf0acdb7143
Wboev <113057943+Wboev@users.noreply.github.com>
2023-01-29 13:46:04 +0300

    Add files via upload

---
+++ HW JS_functions.js
@@ -0,0 +1,41 @@
+"HW JS_functions.js"
+  
+// function f1(p1,p2){ 
+//   let a = p1 = 10; 
+//   let b = p2 = 5; 
+//   result="sum=" + p1 + p2 ; 
+//   console.log(result = p1+p2);
+// }
+// f1() 
+
+
+
+
+// function f2(p1){ 
+//     let a = p1 = 5; 
+//     result = "square number 5 = ";
+//     console.log(result = "square number 5 = "+ p1*p1);
+// } 
+// f2() 
+ 
+  
+
+// function nsm(name,surname,middlename) {
+//     name = "Wenzel" ;
+//   console.log(name);  
+//   surname = "Boev"; 
+//      console.log(surname)
+//      middlename = "Fargooo"; 
+//      console.log(middlename)
+// } 
+// nsm() 
+ 
+function mass(mass) {
+     mass = [0,1,2,3,4,5,6,7,8,9]; 
+} 
+ function view(mass) { 
+    mass * 2 
+    console.log(mass*2)
+    
+ } 
+ view()
